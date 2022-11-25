d5eb5c33c378dde382de793f9cc488f652709e02
Rafael <faelmota7@gmail.com>
2022-11-25 20:16:45 -0300

    25/11

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+Console.WriteLine("-Exemplo de Sub-rotina-");
+
+Console.Write("Digite seu nome......: ");
+Console.ReadKey();
+
+// Lógica de validação do nome
+// ....
+exibirMensagemDeErro("Nome inválido. O nome é obrigatório.");
+
+Console.Write("Digite seu idade.....: ");
+Console.ReadKey();
+
+exibirMensagemDeErro("A idade deve estar entre 0 e 150 anos.");
+
+Console.Write("Digite seu profissão.: ");
+Console.ReadKey();
+
+exibirMensagemDeErro("A profissão é obrigatória.");
+
+
+// Sub-rotina do tipo PROCEDIMENTO com passagem de parâmetro
+
+void exibirMensagemDeErro(string mensagem) // Assinatura
+{
+    
+    string mensagemAjustada = $"| {mensagem} |";
+    int quantidaDeTracos = mensagemAjustada.Length;
+    string barra = "".PadRight(quantidaDeTracos, '-');
+
+    Console.WriteLine();
+    Console.WriteLine(barra);
+    Console.Write("| ");
+    Console.ForegroundColor= ConsoleColor.DarkRed;
+    Console.Write(mensagem);
+    Console.ResetColor();
+    Console.WriteLine(" |");
+    Console.WriteLine(barra);
+    Console.WriteLine();
+    Console.Beep();
+    Console.Beep();
+}
+
+
+//     Console.WriteLine();
+//    Console.WriteLine("------------------------------");
+//    Console.Write("|");
+//    Console.ForegroundColor= ConsoleColor.DarkRed;
+//    Console.Write("Erro ao obter as informações");
+//    Console.ResetColor();
+//    Console.WriteLine("|");
+//    Console.WriteLine("------------------------------");
+//    Console.WriteLine();
+//    Console.Beep();
+//    Console.Beep();
+
+
